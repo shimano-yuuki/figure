cc592563dcfcd3cdadb64a19a2f1c40d5ba3f1a3
simanoyuki <yuukireirei5@gmail.com>
2024-09-28 23:50:30 +0900

    CustomTextFieldの修正

--- lib/presentation/components/custom_text_form_field.dart
+++ lib/presentation/components/custom_text_form_field.dart
@@ -2,31 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:training_app/presentation/style/colors.dart';
 import 'package:training_app/presentation/style/style.dart';
 
-///TODO :カスタムテキストフィールドに移行する。
 class CustomTextFormField extends StatefulWidget {
   const CustomTextFormField({
     super.key,
     required this.labelText,
     this.validate,
+    this.onChanged,
   });
   final String labelText;
   final String? Function(String?)? validate;
+  final Function(String)? onChanged;
 
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
 }
 
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
-  final _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-        style: AppTextstyle.regular(color: MyColor.darkYellow),
-        controller: _controller,
-        decoration: InputDecoration(
-          labelText: widget.labelText,
-          border: const UnderlineInputBorder(),
-        ),
-        validator: widget.validate);
+      style: AppTextstyle.regular(color: MyColor.darkYellow),
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        border: const UnderlineInputBorder(),
+      ),
+      validator: widget.validate,
+      onChanged: widget.onChanged, // onChangedを使用
+    );
   }
 }
