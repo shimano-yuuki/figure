36c6444e8f0ffe3e81d93ec17c1468b8fa7fef7b
simanoyuki <yuukireirei5@gmail.com>
2024-10-06 20:37:25 +0900

    画面の作成

--- lib/core/extention/string_extention.dart
+++ lib/core/extention/string_extention.dart
@@ -1,13 +1,13 @@
 extension StringExtension on String {
   /// 文字列がメールアドレスであるか判定する
-  /// マッチしていたら、trueしていなかったらfalseを返却する。
   bool get emailFormValidation {
     //mailの形式チェック
     return RegExp(r'^.+@[a-zA-Z]+\.[a-zA-Z]+(\.?[a-zA-Z]+)$').hasMatch(this);
   }
 
+  //文字列がパスワードの形式を満たすかを判定する。
   bool get passFormWordValidation {
-    //パスワード形式チェック
-    return RegExp(r'^(?=.*[A-Z])(?=.*[a-z]).{8,}$').hasMatch(this);
+    // パスワード形式チェック（空白文字を含まない）大文字小文字数字それぞれ一つずつ必須
+    return RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?!.*\s).{8,}$').hasMatch(this);
   }
 }
--- lib/presentation/app_router.dart
+++ lib/presentation/app_router.dart
@@ -1,17 +1,26 @@
 import 'package:go_router/go_router.dart';
 import 'package:training_app/presentation/page/home/home_page.dart';
 import 'package:training_app/presentation/page/log_in.dart';
+import 'package:training_app/presentation/page/sign_in_page.dart';
 
 final router = GoRouter(
   debugLogDiagnostics: true,
   initialLocation: '/',
   routes: [
     GoRoute(
-        name: 'sign_in', path: '/', builder: (context, state) => LogInPage()),
+      name: 'log_in',
+      path: '/',
+      builder: (context, state) => LogInPage(),
+    ),
     GoRoute(
       name: 'home',
       path: '/home',
       builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      name: 'sign_in_page',
+      path: '/sign_in_page',
+      builder: (context, state) => SignInPage(),
     )
   ],
 );
